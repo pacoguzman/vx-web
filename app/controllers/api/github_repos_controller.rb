5ea6ff35889ba9595d83c70c6e14c8a1b1aefc3c
Dmitry Galinsky <dima.exe@gmail.com>
2013-10-06 19:39:55 +0400

    add specs for api/github_repos

--- app/controllers/api/github_repos_controller.rb
+++ app/controllers/api/github_repos_controller.rb
@@ -1,33 +1,26 @@
 class Api::GithubReposController < Api::BaseController
 
+  respond_to :json
+
   def index
-    respond_to do |want|
-      want.json { render json: github_repos, each_serializer: GithubRepoSerializer }
-    end
+    respond_with(github_repos, each_serializer: GithubRepoSerializer)
+  end
+
+  def sync
+    current_user.sync_github_repos!
+    respond_with(github_repos, each_serializer: GithubRepoSerializer, location: nil)
   end
 
   def subscribe
     github_repo.subscribe do |new_project|
       new_project.publish :created
     end
-
-    respond_to do |want|
-      want.json { render json: github_repo, serializer: GithubRepoSerializer }
-    end
+    respond_with(github_repo, serializer: GithubRepoSerializer, location: nil)
   end
 
   def unsubscribe
     github_repo.unsubscribe
-    respond_to do |want|
-      want.json { render json: github_repo, serializer: GithubRepoSerializer }
-    end
-  end
-
-  def sync
-    current_user.sync_github_repos!
-    respond_to do |want|
-      want.json { render json: github_repos, each_serializer: GithubRepoSerializer }
-    end
+    respond_with(github_repo, serializer: GithubRepoSerializer, location: nil)
   end
 
   private
---
+++ spec/controllers/api/github_repos_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe Api::GithubReposController do
+  let(:user) { create :user }
+  let(:repo) { create :github_repo, user: user }
+  subject { response }
+
+  before do
+    session[:user_id] = user.id
+  end
+
+  context "GET /index" do
+    before do
+      repo
+      get :index, format: :json
+    end
+    it { should be_success }
+    its(:body) { should_not be_empty }
+  end
+
+  context "POST /sync" do
+    before do
+      repo
+      mock(User).find_by(id: user.id) { user }
+      mock(user).sync_github_repos! { true }
+      post :sync, format: :json
+    end
+    it { should be_success }
+    its(:body) { should_not be_empty }
+  end
+
+  context "POST /subscribe" do
+    before do
+      post :subscribe, id: repo.id, format: :json
+    end
+
+    it { should be_success }
+    its(:body) { should_not be_empty }
+  end
+
+  context "POST /unsubscribe" do
+    before do
+      post :unsubscribe, id: repo.id, format: :json
+    end
+    it { should be_success }
+    its(:body) { should_not be_empty }
+  end
+end
