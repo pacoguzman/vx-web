bb65456edf187a398081ffcc8e578ac0b56b5f18
Dmitry Galinsky <dima.exe@gmail.com>
2013-11-21 23:48:58 +0400

    add ajax-loaders

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,9 +3,6 @@ class Api::BaseController < ApplicationController
   before_filter :default_format_json
 
   def default_format_json
-
-    sleep 0.5
-
     if(request.headers["HTTP_ACCEPT"].nil? &&
        params[:format].nil?)
       request.format = "json"
