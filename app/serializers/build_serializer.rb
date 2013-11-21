011568c420b56bc0b72be13151d915cfe020297c
Dmitry Galinsky <dima.exe@gmail.com>
2013-11-21 16:28:43 +0400

    remove unused BuildStatusSerializer

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -1,5 +1,10 @@
-class BuildSerializer < BuildStatusSerializer
-  attributes :sha, :branch, :author, :author_email, :message, :http_url
+class BuildSerializer < ActiveModel::Serializer
+  attributes :id, :project_id, :number, :status, :started_at, :finished_at,
+             :sha, :branch, :author, :author_email, :message, :http_url
+
+  def status
+    object.status_name
+  end
 
   def branch
     object.branch_label || object.branch
--- app/serializers/build_status_serializer.rb
+++
@@ -1,7 +0,0 @@
-class BuildStatusSerializer < ActiveModel::Serializer
-  attributes :id, :project_id, :number, :status, :started_at, :finished_at
-
-  def status
-    object.status_name
-  end
-end
