97a1456fff7b8a9bcefde8ddda85c47918fc4534
Dmitry Galinsky <dima.exe@gmail.com>
2014-01-29 02:04:55 +0400

    create spec for github/user_sessions_controller

--- app/controllers/github/user_sessions_controller.rb
+++ app/controllers/github/user_sessions_controller.rb
@@ -3,10 +3,10 @@ class Github::UserSessionsController < ApplicationController
   skip_before_filter :authorize_user
 
   def create
-    @github_user_session = Github::UserSession.new env["omniauth.auth"]
-    @user = @github_user_session.create
-    if @user
-      session[:user_id] = @user.id
+    github_user_session = Github::UserSession.new request.env["omniauth.auth"]
+    user = github_user_session.create
+    if user
+      session[:user_id] = user.id
       redirect_to root_path
     else
       redirect_to '/auth/failure'
