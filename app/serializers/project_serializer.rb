a29d6519a5846e795697df72477136dca092789c
Dmitry Galinsky <dima.exe@gmail.com>
2014-01-29 15:02:33 +0400

    add provider_title to projects serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,10 +1,15 @@
 class ProjectSerializer < ActiveModel::Serializer
   cached
 
-  attributes :id, :name, :http_url, :description, :status, :last_build_created_at
+  attributes :id, :name, :http_url, :description, :status, :last_build_created_at,
+    :provider_title
 
   def status
     object.last_build_status
+  end
+
+  def provider_title
+    object.user_repo.try(:provider_title)
   end
 
   def last_build_created_at
