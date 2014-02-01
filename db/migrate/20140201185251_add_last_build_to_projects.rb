2cb06cf45c62dd1975fb665955caca80bbf94d4b
Dmitry Galinsky <dima.exe@gmail.com>
2014-02-01 23:27:29 +0400

    cache project last_build

--- db/migrate/20140201185251_add_last_build_to_projects.rb
+++ db/migrate/20140201185251_add_last_build_to_projects.rb
@@ -1,7 +1,7 @@
 class AddLastBuildToProjects < ActiveRecord::Migration
   def change
     add_column :projects, :last_build_id,          :integer
-    add_column :projects, :last_build_status_name, :integer
+    add_column :projects, :last_build_status_name, :string
     add_column :projects, :last_build_at,          :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140201185251) do
+ActiveRecord::Schema.define(version: 20140201191050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(version: 20140201185251) do
     t.datetime "updated_at"
     t.integer  "user_repo_id"
     t.integer  "last_build_id"
-    t.integer  "last_build_status_name"
+    t.string   "last_build_status_name"
     t.datetime "last_build_at"
   end
 
