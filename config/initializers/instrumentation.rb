a8030cc279d5cebf9d1f48c13d150bddd01707d6
Dmitry Galinsky <dima.exe@gmail.com>
2014-02-01 23:54:10 +0400

    add ENV[VX_COMPONENT_NAME]

--- config/initializers/instrumentation.rb
+++ config/initializers/instrumentation.rb
@@ -1,7 +1,7 @@
 require Rails.root.join("app/middlewares/handle_exception")
 Rails.application.config.middleware.insert 0, Vx::Web::HandleException
 
-VX_APP_NAME ||= ENV['VX_APP_NAME'] || "web"
+VX_COMPONENT_NAME ||= ENV['VX_COMPONENT_NAME'] || "web"
 
-Vx::Instrumentation.install "#{Rails.root}/log/#{VX_APP_NAME}.#{Rails.env}.json"
+Vx::Instrumentation.install "#{Rails.root}/log/#{VX_COMPONENT_NAME}.#{Rails.env}.json"
 
