d96f134dd33a0ba6d5ad0f07279c14b978a1f9c7
Dmitry Galinsky <dima.exe@gmail.com>
2014-02-05 19:59:53 +0400

    ignore JobLogsConsumer ServerSideEventsConsumer in instrumentation

--- app/instrumentations/vx_consumer.rb
+++ app/instrumentations/vx_consumer.rb
@@ -4,7 +4,14 @@ require 'vx/instrumentation'
 $stdout.puts ' --> initializing ActiveSupport::Notifications for vx-consumer'
 
 ActiveSupport::Notifications.subscribe(/\.consumer\.vx/) do |event, started, finished, _, payload|
-  Vx::Instrumentation.delivery event, payload, event.split("."), started, finished
+  ingnored_consumers = %w{
+    ServerSideEventsConsumer
+    JobLogsConsumer
+  }
+
+  unless ingnored_consumers.include?(payload[:consumer])
+    Vx::Instrumentation.delivery event, payload, event.split("."), started, finished
+  end
 end
 
 ActiveSupport::Notifications.subscribe(/\.consumer\.web\.vx/) do |event, started, finished, _, payload|
