de2a76250ad1ebdf1a6cf2ca796dc870cae84a25
Paco Guzman <pacoguzmanp@gmail.com>
2014-05-23 13:23:06 +0200

    Shows project info on the queue

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -2,7 +2,12 @@ class BuildSerializer < ActiveModel::Serializer
   cached
 
   attributes :id, :project_id, :number, :status, :started_at, :finished_at,
-             :sha, :branch, :author, :author_email, :message, :http_url, :pull_request_id
+             :sha, :branch, :author, :author_email, :message, :http_url, :pull_request_id,
+             :project_name
+
+  def project_name
+    object.project.name
+  end
 
   def status
     object.status_name
