a145a921f14dbba1d70b855e6f15faff7b7dea8b
Dmitry Galinsky <dima.exe@gmail.com>
2014-06-09 03:15:06 +0400

    create serializer for companies and include it to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
   cached
 
-  attributes :id, :email, :name, :project_subscriptions
+  attributes :id, :email, :name, :project_subscriptions, :default_company
 
   has_many :identities
+  has_many :companies
 
   def project_subscriptions
     object.project_subscriptions.active.map do |s|
       s.project_id
     end
+  end
+
+  def default_company
+    object.default_company.try(:id)
   end
 end
