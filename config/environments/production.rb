f8eb113dd5fc3abc02d31dcace4c29311d776245
Dmitry Galinsky <dima.exe@gmail.com>
2014-07-06 18:43:49 +0400

    Revert "enable mangle in assets compilation"
    This reverts commit 89cc5d199ba2bdf801d384a5206dd85cd326a23f.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ VxWeb::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.js_compressor = Uglifier.new(mangle: false)
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
